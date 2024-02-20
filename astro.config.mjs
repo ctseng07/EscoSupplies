12a37548a47ee1f80a3c58cc583610055609c9ee
Caleb Tseng <calebtseng07@gmail.com>
2024-02-19 17:39:45 -0800

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://ctseng07.github.io',
+  base: '/EscoSupplies',
+  integrations: [tailwind()]
+});
