7feef8aaf7e90c9325107fe0d4e2d904dc3fc0e7
Caleb Tseng <calebtseng07@gmail.com>
2024-02-19 17:50:51 -0800

    Add deploy page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [tailwind()],
   site: 'https://ctseng07.github.io',
   base: '/EscoSupplies',
-  integrations: [tailwind()]
 });
