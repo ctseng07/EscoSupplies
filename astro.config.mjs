008bdee0f1f944fedeb7e63519b7498fb4cfbc20
Samson Cheung <samsonc89@yahoo.com>
2024-08-09 08:35:21 -0700

    Update tsconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://ctseng07.github.io',
-  base: '/EscoSupplies',
+  site: "https://escosupplies.com",
+  base: "/",
 });
